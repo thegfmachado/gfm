6f7c56c93a559afd25c2e340369b2ab0dc3e1869
thegfmachado <gabriel.ferreira.machado@hotmail.com>
2024-12-28 17:41:06 -0300

    feat(projects): adding projects page content

---
+++ src/constants/projects.ts
@@ -0,0 +1,16 @@
+export const projectsInfo = {
+  lofiDino: {
+    logo: "/images/projects/lofiDino.png",
+    repo: "https://github.com/thegfmachado/lofi-dino",
+    site: "https://lofi-dino.vercel.app/",
+  },
+  whatDoesTheFoxHad: {
+    logo: "/images/projects/whatDoesTheFoxHad.png",
+    site: "https://what-does-the-fox-had.vercel.app/",
+    repo: "https://github.com/thegfmachado/what-does-the-fox-had",
+  },
+  gfm: {
+    logo: "/images/projects/gfm.png",
+    repo: "https://github.com/thegfmachado/gfm",
+  },
+};
